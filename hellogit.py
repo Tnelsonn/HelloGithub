c26f5b0526e542f690c3387128bd85bf34bc6cd1
Tnelsonn <topher4403@gmail.com>
2020-02-06 09:45:46 -0600

    add while loop

--- hellogit.py
+++ hellogit.py
@@ -46,6 +46,12 @@ mylist = [1, 3, 8, 412, 43, 2, 20]
 #length of list
 len(mylist)
 
+#whileloop
+
+x=0
+while(x<5)
+ x=x+1
+print ("x = "str(x))
 #for loop by index
 for i in range(len(mylist)):
 	print(mylist[i])
